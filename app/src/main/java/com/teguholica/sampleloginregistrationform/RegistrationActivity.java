7de77927bd6e3b105bed7f05f01ca56a4f7f531e
Teguh Arifianto <teguholica@gmail.com>
2017-01-07 11:24:19 +0700

    Initial commit

---
+++ app/src/main/java/com/teguholica/sampleloginregistrationform/RegistrationActivity.java
@@ -0,0 +1,26 @@
+package com.teguholica.sampleloginregistrationform;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLinkToLoginScreen);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
